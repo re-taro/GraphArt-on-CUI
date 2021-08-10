e0b175221d014fe6aebde134f365a74129a6c67b
Re-taro <aminokacho@gmail.com>
2021-08-10 23:20:14 +0900

    [update]

--- graphart/graphart.py
+++ graphart/graphart.py
@@ -3,31 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import copy
+from classFile import Expression as exp
 
 
-class Expression:
-    def __init__(self):
-        self.
+PI = np.pi
+
 
 def setup(numOfExp):
-
+    for i in range(numOfExp):
+        
 
 def drawGraph():
-    canvas = plt.figure(figsize = (1.920, 1.080),
-                        dpi = 1000,
+    canvas = plt.figure(figsize = (4, 4),
+                        dpi = 100,
                         facecolor = 'w')
-    
+
 
 
 
 def main():
-    print('README.PDFを読みましたか？')
-    readFlag = input('Y/n ')
+    print('README.pdfを読みましたか？')
+    readFlag = input('Y/n: ')
     if readFlag == 'Y' or readFlag == 'y':
         print('入力する式の数を入力してください。')
         numOfExp = input('ここに入力してください: ')
     elif readFlag == 'n' or readFlag == 'N':
-        print('もう一度目を通してから再度プログラムを起動してください。')
+        print('READMEへもう一度目を通してから再度プログラムを起動してください。')
         sys.exit()
     else:
         print('Yかnで答えてください。')
