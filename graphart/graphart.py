65fe12c1d724dee9486d2b2407b3338dd840bd37
Re-taro <aminokacho@gmail.com>
2021-08-11 13:24:10 +0900

    [update] Implement setup function

--- graphart/graphart.py
+++ graphart/graphart.py
@@ -3,15 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import copy
-from classFile import Expression as exp
+from classFile import ExpressionTemplate as expTemp
 
 
 PI = np.pi
+exp = []
 
 
 def setup(numOfExp):
     for i in range(numOfExp):
-        
+        exp[i] = expTemp()
+
+        print(i + 1 + '番目の式を設定していきます。')
+        typeExp = input('式の種類を入力してください: ')
+        minX = input('xの最小値を入力してください: ')
+        maxX = input('xの最大値を入力してください: ')
+        minY = input('yの最小値を入力してください: ')
+        maxY = input('yの最大値を入力してください: ')
+        dnum = input('分割数を入力してください: ')
+        formExp = input('式を入力してください: ')
+
+        exp[i].expRule(typeExp, minX, maxX, minY, maxY, dnum, formExp)
 
 def drawGraph():
     canvas = plt.figure(figsize = (4, 4),
